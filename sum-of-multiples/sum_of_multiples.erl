2bd8633a9b0caf96bc931c41d461961755976872
Emir Ozer <emir.ozer@mendix.com>
2016-12-13 11:37:01 +0100

    sum-of-multiples all tests pass, handling of duplicates

--- sum-of-multiples/sum_of_multiples.erl
+++ sum-of-multiples/sum_of_multiples.erl
@@ -11,13 +11,15 @@ sumOfMultiples(Multiples, _) when Multiples == []->
 sumOfMultiples(Multiples, Range) ->
     Sequence  = [X || X <- lists:seq(1, Range - 1)],
     DivisibleSequence = get_divisible_sequence(Sequence, Multiples, []),
-    sum(DivisibleSequence).
+    sum(remove_duplicates_from_list(DivisibleSequence)).
 
+%% This solution with removing duplicates before summing works fine
+%% should make it that there are no duplicates, in the second iteration
 get_divisible_sequence(Sequence, Multiples, Summables) ->
     [H | T] = Multiples,
     NewSequence  = [X || X <- Sequence, X rem H == 0 ],
     if
-        T =/= [] -> get_divisible_sequence(NewSequence, T, Summables ++ NewSequence);
+        T =/= [] -> get_divisible_sequence(Sequence, T, Summables ++ NewSequence);
         true -> Summables ++ NewSequence
     end.
 
@@ -30,3 +32,6 @@ sum([H|T], Acc) ->
 
 sum([], Acc) ->
     Acc.
+
+remove_duplicates_from_list(X) ->
+    sets:to_list(sets:from_list(X)).
